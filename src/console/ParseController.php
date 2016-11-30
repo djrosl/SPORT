642a2b880b439843a8f76e06518b835a29851df9
djrosl <dubztep45@gmail.com>
2016-11-30 15:52:02 +0200

    dump usda db

--- src/console/ParseController.php
+++ src/console/ParseController.php
@@ -41,29 +41,29 @@ class ParseController extends Controller {
         $data = \moonland\phpexcel\Excel::import(Yii::getAlias('@webroot').'/'.$file);
 
         foreach($data as $product){
-            $desc = str_replace(',', ', ', $product['Shrt_Desc']);
+            /*$desc = str_replace(',', ', ', $product['Shrt_Desc']);
             //echo $desc;break;
-            $model = Product::findOne(['title_en'=>$desc]);
-            if(!$model){
+            $model = Product::findOne(['title_en'=>$desc]);*/
+            /*if(!$model){*/
                 $replaced = 'NEW';
                 $model = new Product();
                 $model->title_en = $product['Shrt_Desc'];
                 $model->save();
-            } else {
+            /*} else {
                 $replaced = 'REPLACED';
-            }
+            }*/
             foreach($product as $key => $value){
                 $nutrient = TestNutrient::findOne(['usda'=>$key]);
                 if($nutrient){
-                    $pn = ProductNutrient::findOne(['product_id'=>$model->id, 'nutrient_id'=>$nutrient->id]);
-                    if(!$pn){
+                    /*$pn = ProductNutrient::findOne(['product_id'=>$model->id, 'nutrient_id'=>$nutrient->id]);
+                    if(!$pn){*/
                         $pn = new ProductNutrient();
                         $pn->product_id = $model->id;
                         $pn->nutrient_id = $nutrient->id;
-                    }
+                    /*}*/
                     $val = (float)str_replace(',', '.', $value);
-                    if($val || $pn->value){
-                        $pn->value = $val > $pn->value ? $val : $pn->value;
+                    if($val/* || $pn->value*/){
+                        $pn->value = $val /*> $pn->value ? $val : $pn->value*/;
                         $pn->save();
                     }
                 }
