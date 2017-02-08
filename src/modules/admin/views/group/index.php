0078aa1ad1050304b3232d1815d28a831dce2944
djrosl <dubztep45@gmail.com>
2017-02-08 15:18:59 +0200

    exercise filter

--- src/modules/admin/views/group/index.php
+++ src/modules/admin/views/group/index.php
@@ -131,7 +131,7 @@ $this->title = 'Поиск / добавление в группу'; ?>
             <?php foreach($groupModels as $model): ?>
                 <tr>
                     <td><label for="p<?= $model['id'] ?>"><?=$model['title_en']?></label></td>
-                    <td><?php if(ProductToGroup::findOne(['product_id'=>$model['id']])->group): ?><a href="<?= Url::to(['view', 'id'=>ProductToGroup::findOne(['product_id'=>$model['id']])->group->id]) ?>"><?=ProductToGroup::findOne(['product_id'=>$model['id']])->group->title?></a><?php endif; ?></td>
+                   <?php /*  <td><?php  if(ProductToGroup::findOne(['product_id'=>$model['id']])->group): ?><a href="<?= Url::to(['view', 'id'=>ProductToGroup::findOne(['product_id'=>$model['id']])->group->id]) ?>"><?=ProductToGroup::findOne(['product_id'=>$model['id']])->group->title?></a><?php endif; ?></td>  */ ?>
                     <td><?=$model['ndb_slug']?></td>
                 </tr>
             <?php endforeach; ?>
