50a3f8de009a2fae2969ecada760541849368234
djrosl <dubztep45@gmail.com>
2017-02-13 12:44:59 +0200

    fixes

--- src/modules/admin/views/group/view.php
+++ src/modules/admin/views/group/view.php
@@ -79,7 +79,7 @@ $array = ArrayHelper::map( ProductGroup::find()->all(), 'id', 'title');
             $query = $group->getProducts()->andFilterWhere(['or','related_id is NULL',['=','related_id',0]]);
             $anotherDataProvider = false;
         } else {
-            $query = $group->getProducts()->where(['like','title_en', $search])->andFilterWhere(['or',['is','related_id',NULL],['=','related_id',0]]);
+            $query = $group->getProducts()->where(['like','title_en', $search])->andFilterWhere(['or','related_id is NULL',['=','related_id',0]]);
 
             $another_query = ProductToGroup::find()->joinWith('product')->where(['like','title_en', $search])->andWhere(['!=','group_id',$group->id])->orderBy('group_id');
 
